d88a1094b4127b2dcac1e1ee2a8d2f4064ad66d8
helmdyck <helmuth.dyck@gmail.com>
2023-12-30 19:59:31 -0300

    Hero added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import mybloglogo from "../assets/mybloglogo.png";
-import menuPlus from "../assets/menuPlus.png";
 import "../App.css";
 
 function Navigation() {
+  const [showMenu, setShowMenu] = useState(false);
+  console.log(showMenu);
+
   return (
     <>
       <div className="nav-container">
-        <div className="logo">
-          <img src={mybloglogo} alt="My Blog Logo" className="main-logo" />
-        </div>
-        <label className="plus-menu">
-          <input type="checkbox" />
-          <div className="bar">
-            <span className="vertical"></span>
-            <span className="horizontal"></span>
+        <div className="nav-inner">
+          <div className="logo">
+            <img src={mybloglogo} alt="My Blog Logo" className="main-logo" />
           </div>
-        </label>
+
+          <label className="plus-menu" onClick={() => setShowMenu(!showMenu)}>
+            <input type="checkbox" />
+            <div className="bar">
+              <span className="vertical"></span>
+              <span className="horizontal"></span>
+            </div>
+          </label>
+          {showMenu && (
+            <div className="menu-container">
+              <ul>
+                <li>Home</li>
+                <li>Categories</li>
+                <li>Creators</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="container-fluid hero-container">
+        <h1 className="hero-title">
+          La sexualidad es una fuente de placer y felicidad, <br /> aquí te
+          ayudaremos a descubrirla.
+        </h1>
+      </div>
+      <div className="section">
+        <div className="container categories-container">
+          <div className="category-box">
+            <button className="btn rounded-pill">TODO</button>
+          </div>
+          <div className="category-box">
+            <button className="btn rounded-pill">JUGUETES</button>
+          </div>
+          <div className="category-box">
+            <button className="btn rounded-pill">SALUD SEXUAL</button>
+          </div>
+          <div className="category-box">
+            <button className="btn rounded-pill">HIGIENE ÍNTIMA</button>
+          </div>
+
+          {/* <div className="row tex-center">
+            <div className="btn category-box">ALL</div>
+            <div className="btn category-box">JUGUETES</div>
+            <div className="btn category-box">SALUD SEXUAL</div>
+            <div className="btn category-box">HIGIENE ÍNTIMA</div>
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
